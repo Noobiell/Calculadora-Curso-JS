4a9bd67e030c8ab2909173e8b799ca6bcbd6db96
Gabriel de Lima Oliveira <75146115+Noobiell@users.noreply.github.com>
2021-11-19 14:21:03 -0300

    manipulando_array_para_concatenar_numeros

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -39,8 +39,44 @@ class CalcController {
         this._operation.pop()
     }
 
+    getLastOperation(){
+
+        return this._operation[this._operation.length-1];
+
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length-1] = value;
+    }
+
+    isOperator(value){
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);// O INDEXOFF PESQUISA O VALOR DENTRO DO ARRAI E VERIFICA SE EXISTE LÁ, SE EXISTE ELE TRAZ A POSIÇÃO DO ARRAY
+    }
+
     addOperator(value){
-        this._operation.push(value);
+
+        if(isNaN(this.getLastOperation())){
+            //quando string
+            if(this.isOperator(value)){
+                //trocar o operador
+                this.setLastOperation() = value;
+
+            }else if(isNaN(value)){
+                
+                //outra coisa
+                console.log(value);
+
+            }else{
+
+                this._operation.push(value);
+
+            }
+        }else{
+            //quando numero
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+
         console.log(this._operation);
     }
 
@@ -57,22 +93,25 @@ class CalcController {
                 this.clearEntry();
                 break;
             case 'soma':
-                
+                this.addOperator('+');
                 break;
             case 'subtracao':
-
+                this.addOperator('-');
                 break;
             case 'divisao':
-
+                this.addOperator('/');
                 break;
             case 'multiplicacao':
-
+                this.addOperator('*');
                 break;
             case 'porcento':
-
+                this.addOperator('%');
                 break;
             case 'igual':
 
+                break;
+            case 'ponto':
+                this.addOperator('.');
                 break;
 
             case '0':
